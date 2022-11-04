f87f30e5320dbf5fa2fbb798caceee53f0e26759
Carto-NA <63336882+Carto-NA@users.noreply.github.com>
2022-11-04 09:01:43 +0100

    Create 01_eco_tiers_lieux.py
    Initialisation du fichier

---
+++ scripts/01_eco_tiers_lieux.py
@@ -0,0 +1,15 @@
+#-------------------------------------------------------------------------------
+# Name:        API_gogocarto
+# Purpose:     Script qui permet d'interroger l'API, de récuperer le résultat au format JSON
+#              et de le reconstruire pour qu'il soit exploitable au format tableau
+#
+# Author:      vincentto
+#
+# Created:     03/11/2022
+# Copyright:   (c) vincentto 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# coding: utf-8
